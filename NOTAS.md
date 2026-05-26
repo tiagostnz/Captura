d7e1f70f51e47398c4a92ccb2a324fa4d0e5c44b
tiagosntz <borgesdossantost0@gmail.com>
2026-05-26 15:18:36 -0300

    configurei o next-auth v5 com o credentials provider

--- NOTAS.md
+++ NOTAS.md
@@ -7,4 +7,12 @@ arquivo package.json:
 
 migrations:
 - os arquivos são basicamente os mesmos, oq muda é que tem nomes diferentes para cada migration de cada função
+- migration são códigos que descrevem mudanças no schema, executadas em uma vez só, em ordem, com histórico no próprio banco
+- schema é a estrutura do banco de dados, a definição de quais tabelas existem, quais colunas elas tem, tipos, índices, chaves estrangeiras, etc.
+- não dá pra editar uma migration dps que ela já rodou
+
+next-auth:
+- pra começar baixar a v5 pois estou usando o app router + next 16, se n ia ser uma dor de cabeça(ele é especifico pra isso)
+- tem um sistema de Autenticação VS Autorização - o Auth cuida da autenticação, e as permissões é o adm que define
+- quando um usuário loga, o servidor cria uma sessão e devolve no cookie, então, cada request seguinte, o navegador manda esse cookie de volta 
 
---
+++ auth.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+
+// esse bloco chama a função NextAuth que devolve um objeto com 4 funções e exporta todas de uma vez usando destructuring assignment, ou seja, pega as 4 funções do objeto e exporta cada uma com seu nome, pra usar em outras partes do app
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+  credentials: {
+    email: {},
+    password: {},
+  },
+
+//basicamente procura na tabela users alguém com aquele email
+  async authorize(credentials) {
+    const user = await db("users")
+      .where({ email: credentials.email as string })
+      .first();
+
+    if (!user) return null;
+// o bcrpt.compare pega a senha que o usuário digitou e compara com a senha criptografada que tá no banco de dados, devolve true se for igual ou false se for diferente
+    const senhaOk = await bcrypt.compare(
+      credentials.password as string,
+      user.password_hash
+    );
+
+    if (!senhaOk) return null;
+// se ambos estivem ok, devolve um objeto com as informações do usuário
+    return {
+      id: String(user.id),
+      email: user.email,
+      name: user.name,
+    };
+  },
+}),
+  ],
+});
