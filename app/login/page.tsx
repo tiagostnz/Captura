ad5d59e14c0c7ce12ee05c43824b3e75b5bf2f78
tiagosntz <borgesdossantost0@gmail.com>
2026-05-26 17:19:32 -0300

    implementei o login com credentials provider

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,24 @@
-export default function Login() {
+import { signIn } from "@/auth";
+
+async function login(formData: FormData) {
+  "use server";
+
+  await signIn("credentials", {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+    redirectTo: "/",
+  });
+}
+
+export default function LoginPage() {
   return (
     <div>
       <h1>Login</h1>
+      <form action={login}>
+        <input name="email" type="email" placeholder="Email" required />
+        <input name="password" type="password" placeholder="Senha" required />
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
 }
