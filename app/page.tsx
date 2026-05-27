e60547351673087b253a58c27b90d5121ec49e71
tiagosntz <borgesdossantost0@gmail.com>
2026-05-27 15:48:00 -0300

    adicionei o logout e a exibição de sessão na home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+import { auth, signOut } from "@/auth";
 import { db } from "@/lib/db";
 
-export default async function Home() {
-  const users = await db("users").select("*");
+async function logout() {
+  "use server";
+  await signOut({ redirectTo: "/login" });
+}
 
+export default async function Home() {
+  const session = await auth();
+  const users = await db("users").select("*");
+// é oq aparece quando vc loga no sistema
   return (
     <div>
+      {session ? (
+        <div>
+          <p>Logado como: {session.user?.name} ({session.user?.email})</p>
+          <form action={logout}> 
+            <button type="submit">Sair</button>
+          </form>
+        </div>
+        // é usado o ? para se caso o user não existir, mostrar que n é definido
+      ) : (
+        <p>
+          Não logado. <a href="/login">Fazer login</a>
+        </p>
+      )}
+
       <h1>Usuários no banco:</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
