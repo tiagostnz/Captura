1cbd2bc3e3277d11b20ff2d202a08a5eff7c775a
tiagosntz <borgesdossantost0@gmail.com>
2026-06-22 16:34:17 -0300

    arrumei os warnings chatos que tinham

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
     serverActions: {
       // permite uploads maiores que o padrão de 1MB
       bodySizeLimit: "100mb", // coloquei um limite meio alto, mas só pra teste mesmo, cpa mudo dps
+    
+
+      images: {
+  remotePatterns: [
+    { protocol: "https", hostname: "res.cloudinary.com" },
+    { protocol: "https", hostname: "picsum.photos" },
+    { protocol: "https", hostname: "fastly.picsum.photos" },
+  ],
+},
+
     },
   },
 };
